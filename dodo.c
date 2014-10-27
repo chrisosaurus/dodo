974c9d6bf911ef4f1ec5f2d63003f8a74356367d
Chris Hall <followingthepath@gmail.com>
2014-10-27 22:41:42 +1300

    minor update to tagline

--- dodo.c
+++ dodo.c
@@ -528,7 +528,7 @@ EXIT:
 
 /***** main *****/
 void usage(void){
-    puts("dodo - dumb in place file editor\n"
+    puts("dodo - scriptable in place file editor\n"
          "dodo takes a single argument of <filename>\n"
          "and will read commands from stdin\n"
          "\n"
