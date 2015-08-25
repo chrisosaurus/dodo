542e8cc0cc027b8cacecae05445a6199f6f6bcd3
Chris Hall <followingthepath@gmail.com>
2015-08-25 22:14:33 +1200

    updating config.mk for gcov

--- config.mk
+++ config.mk
@@ -6,7 +6,13 @@ MANPREFIX = ${PREFIX}/share/man
 INCS = 
 LIBS = 
 
-CFLAGS = -std=c99 -pedantic -Wall ${INCS}
-LDFLAGS = ${LIBS}
+# NB: including  -fprofile-arcs -ftest-coverage for gcov
+# travis wasn't happy with -Wmaybe-uninitialized  so removed for now
+# -Wextra was removed due to unused params
+CFLAGS = -std=c99 -pedantic -Werror -Wall -Wstrict-prototypes -Wshadow -Wdeclaration-after-statement -Wunused-function -fprofile-arcs -ftest-coverage ${INCS}
+
+# NB: including  -fprofile-arcs for gcov
+LDFLAGS = -fprofile-arcs ${LIBS}
+
 
 CC = cc
