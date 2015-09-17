cbf1989e0774c38cd1f7f9cb4e51ba4a14a539ca
David Phillips <dbphillipsnz@gmail.com>
2015-09-17 14:51:10 +1200

    Added interactive mode test to test.sh

--- test.sh
+++ test.sh
@@ -9,10 +9,12 @@ set -eu
 TESTFILENAME=$(mktemp) || exit
 VALGRINDOPTS="--track-origins=yes --error-exitcode=1 --leak-check=full --show-reachable=yes"
 
-echo -e "\nWriting file"
-cat <<EOF > "$TESTFILENAME"
+make_test_file() {
+    echo -e "\nWriting file"
+    cat <<EOF > "$TESTFILENAME"
 hello world how are you mutter mutter sl/ash
 EOF
+}
 
 warn() {
     if [[ -e $TESTFILENAME ]] ; then
@@ -20,36 +22,43 @@ warn() {
         exit 1
     fi
 }
-trap warn EXIT
-
-echo -e "\nRunning dodo"
-valgrind $VALGRINDOPTS ./dodo "$TESTFILENAME" <<EOF
-    p          # print 100 bytes
-    p5         # print 5 bytes ('hello')
-    e/hello/   # expect string 'hello'
-    b6         # goto byte 6 in file
-    e/world/   # expect string 'world'
-    w/marge/   # write string 'marge' (writes over 'world')
-    b38
-    e/sl\/ash/ # expect string 'sl/ash'
-    w/slashy/  # write over string 'sl/ash' with 'slashy'
-    q          # quit
+do_test() {
+    [ -z "$1" ] && exit
+    echo -e "\nRunning $1"
+    valgrind $VALGRINDOPTS $1 "$TESTFILENAME" <<EOF
+        p          # print 100 bytes
+        p5         # print 5 bytes ('hello')
+        e/hello/   # expect string 'hello'
+        b6         # goto byte 6 in file
+        e/world/   # expect string 'world'
+        w/marge/   # write string 'marge' (writes over 'world')
+        b38
+        e/sl\/ash/ # expect string 'sl/ash'
+        w/slashy/  # write over string 'sl/ash' with 'slashy'
+        q          # quit
 EOF
 
 
-echo -e "\nComparing output"
-GOT=`cat "$TESTFILENAME"`
-EXPECTED="hello marge how are you mutter mutter slashy"
+    echo -e "\nComparing output"
+    GOT=`cat "$TESTFILENAME"`
+    EXPECTED="hello marge how are you mutter mutter slashy"
 
-if [ ! "$GOT" = "$EXPECTED" ]; then
-    echo -e "\nTest failed:"
-    echo "Got '$GOT'"
-    echo "Expected '$EXPECTED'"
-    # do not clean up on failure to allow inspection of file
-    #rm -f -- "$TESTFILENAME"
-    exit 1
-fi
+    if [ ! "$GOT" = "$EXPECTED" ]; then
+        echo -e "\nTest failed:"
+        echo "Got '$GOT'"
+        echo "Expected '$EXPECTED'"
+        # do not clean up on failure to allow inspection of file
+        #rm -f -- "$TESTFILENAME"
+        exit 1
+    fi
+}
 
+trap warn EXIT
+
+for dodo in "./dodo" "./dodo -i"; do
+    make_test_file
+    do_test "$dodo"
+done
 
 echo -e "\nAll green"
 
