775c07baa47d2cbc772b272c212e753a9a562914
Chris Hall <followingthepath@gmail.com>
2020-04-03 18:39:53 +1100

    Adding stdout capture option to exhaustive tests.

--- t/exhaustive.sh
+++ t/exhaustive.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env sh
 
+# foo.dodo   - program given to dodo to run
+# foo.in     - file given to dodo to operatoe on
+# foo.out    - expected file after running dodo
+# foo.stdout - optional, expected stdout produced from running dodo program
+
 for infile in t/tests/exhaustive/*.in; do
     base=`echo $infile | sed 's/\.in$//g'`
     testfile="$base.tmp"
+    teststdout="$base.stdout_tmp"
 
     echo "testing $base"
     cp $infile $testfile
 
-    ./dodo $testfile < "$base.dodo"
+    ./dodo $testfile < "$base.dodo" > "$teststdout"
     ret=$?
     if [ $ret -ne 0 ]; then
         echo "dodo failed: test failed for $base"
@@ -21,7 +27,18 @@ for infile in t/tests/exhaustive/*.in; do
         exit 1
     fi
 
+    expstdout="$base.stdout"
+    if [ -e "$expstdout" ]; then
+        diff $teststdout $expstdout
+        ret=$?
+        if [ $ret -ne 0 ]; then
+            echo "stdout was not expected: test failed for $base"
+            exit 1
+        fi
+    fi
+
     rm $testfile
+    rm $teststdout
 done
 
 echo "exhaustive testing completed successfully"
